daa222b06a687045fd01bad49fab91b8184b0999
Kyle Unverferth <kyle@kyleu.com>
2023-09-26 08:59:08 -0600

    Codegen improvements

--- app/lib/search/generated.go
+++ app/lib/search/generated.go
@@ -16,29 +16,23 @@ import (
 
 func generatedSearch() []Provider {
 	collectionFunc := func(ctx context.Context, params *Params, as *app.State, page *cutil.PageState, logger util.Logger) (result.Results, error) {
-		if !page.Admin {
-			return nil, nil
-		}
 		prm := params.PS.Get("collection", nil, logger).Sanitize("collection").WithLimit(5)
 		models, err := as.Services.Collection.Search(ctx, params.Q, nil, prm, logger)
 		if err != nil {
 			return nil, err
 		}
 		return lo.Map(models, func(m *collection.Collection, _ int) *result.Result {
-			return result.NewResult("collection", m.String(), m.WebPath(), m.String(), "archive", m, m, params.Q)
+			return result.NewResult("collection", m.String(), m.WebPath(), m.TitleString(), "archive", m, m, params.Q)
 		}), nil
 	}
 	itemFunc := func(ctx context.Context, params *Params, as *app.State, page *cutil.PageState, logger util.Logger) (result.Results, error) {
-		if !page.Admin {
-			return nil, nil
-		}
 		prm := params.PS.Get("item", nil, logger).Sanitize("item").WithLimit(5)
 		models, err := as.Services.Item.Search(ctx, params.Q, nil, prm, logger)
 		if err != nil {
 			return nil, err
 		}
 		return lo.Map(models, func(m *item.Item, _ int) *result.Result {
-			return result.NewResult("item", m.String(), m.WebPath(), m.String(), "file", m, m, params.Q)
+			return result.NewResult("item", m.String(), m.WebPath(), m.TitleString(), "file", m, m, params.Q)
 		}), nil
 	}
 	return []Provider{collectionFunc, itemFunc}
