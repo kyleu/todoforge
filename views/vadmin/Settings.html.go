17c47b07c48ab8d4c3c3bddb4dc2623be0fc9031
Kyle Unverferth <kyle@kyleu.com>
2024-05-24 10:25:17 -0600

    Doc improvement

--- views/vadmin/Settings.html.go
+++ views/vadmin/Settings.html.go
@@ -172,7 +172,7 @@ func (p *Settings) StreamBody(qw422016 *qt422016.Writer, as *app.State, ps *cuti
 	qw422016.N().S(`App Profiling</h3>
     `)
 //line views/vadmin/Settings.html:54
-	streamsettingsLink(qw422016, "/admin/memusage", "cog", "Memory Usage", "Detailed memory usage statistics", ps)
+	streamsettingsLink(qw422016, "/admin/memusage", "cog", "Memory Usage", "Detailed memory usage statistics for this application", ps)
 //line views/vadmin/Settings.html:54
 	qw422016.N().S(`
     `)
