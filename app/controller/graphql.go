261a76a12ae2b2394e9d77950899dca0907118b2
Kyle Unverferth <kyle@kyleu.com>
2024-06-24 17:39:25 -0600

    MIME download improvements

--- app/controller/graphql.go
+++ app/controller/graphql.go
@@ -68,6 +68,6 @@ func GraphQLRun(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			return "", err
 		}
-		return cutil.RespondMIME("", "application/json", "json", util.ToJSONBytes(rsp, true), ps.W)
+		return cutil.RespondJSON(ps.W, "", rsp)
 	})
 }
