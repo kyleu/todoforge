fd799ebb7e6739882bbec63be2cb63c5599b74e3
Kyle Unverferth <kyle@kyleu.com>
2024-07-21 10:37:56 -0600

    Generated code improvements and cleanups

--- views/vcollection/List.html.go
+++ views/vcollection/List.html.go
@@ -52,7 +52,11 @@ func (p *List) StreamBody(qw422016 *qt422016.Writer, as *app.State, ps *cutil.Pa
 //line views/vcollection/List.html:23
 	if len(p.Models) > 1 {
 //line views/vcollection/List.html:23
-		qw422016.N().S(`<a href="/collection/_random"><button>`)
+		qw422016.N().S(`<a href="`)
+//line views/vcollection/List.html:23
+		qw422016.E().S(collection.Route(p.Paths...))
+//line views/vcollection/List.html:23
+		qw422016.N().S(`/_random"><button>`)
 //line views/vcollection/List.html:23
 		components.StreamSVGButton(qw422016, "gift", ps)
 //line views/vcollection/List.html:23
--- views/vcollection/vitem/List.html.go
+++ views/vcollection/vitem/List.html.go
@@ -54,7 +54,11 @@ func (p *List) StreamBody(qw422016 *qt422016.Writer, as *app.State, ps *cutil.Pa
 //line views/vcollection/vitem/List.html:25
 	if len(p.Models) > 1 {
 //line views/vcollection/vitem/List.html:25
-		qw422016.N().S(`<a href="/collection/item/_random"><button>`)
+		qw422016.N().S(`<a href="`)
+//line views/vcollection/vitem/List.html:25
+		qw422016.E().S(item.Route(p.Paths...))
+//line views/vcollection/vitem/List.html:25
+		qw422016.N().S(`/_random"><button>`)
 //line views/vcollection/vitem/List.html:25
 		components.StreamSVGButton(qw422016, "gift", ps)
 //line views/vcollection/vitem/List.html:25
