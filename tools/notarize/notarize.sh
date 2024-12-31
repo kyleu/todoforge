ea38959912051f05494506999baa71f8eb9ff544
Kyle Unverferth <kyle@kyleu.com>
2024-12-31 13:29:47 -0700

    v0.1.38

--- tools/notarize/notarize.sh
+++ tools/notarize/notarize.sh
@@ -6,7 +6,7 @@ cd "$dir/../.."
 
 if [ "$PUBLISH_TEST" != "true" ]
 then
-  xcrun notarytool submit --apple-id $APPLE_EMAIL --team-id $APPLE_TEAM_ID --password $APPLE_PASSWORD ./build/dist/todoforge_0.1.37_darwin_amd64_desktop.dmg
-  xcrun notarytool submit --apple-id $APPLE_EMAIL --team-id $APPLE_TEAM_ID --password $APPLE_PASSWORD ./build/dist/todoforge_0.1.37_darwin_arm64_desktop.dmg
-  xcrun notarytool submit --apple-id $APPLE_EMAIL --team-id $APPLE_TEAM_ID --password $APPLE_PASSWORD ./build/dist/todoforge_0.1.37_darwin_all_desktop.dmg
+  xcrun notarytool submit --apple-id $APPLE_EMAIL --team-id $APPLE_TEAM_ID --password $APPLE_PASSWORD ./build/dist/todoforge_0.1.38_darwin_amd64_desktop.dmg
+  xcrun notarytool submit --apple-id $APPLE_EMAIL --team-id $APPLE_TEAM_ID --password $APPLE_PASSWORD ./build/dist/todoforge_0.1.38_darwin_arm64_desktop.dmg
+  xcrun notarytool submit --apple-id $APPLE_EMAIL --team-id $APPLE_TEAM_ID --password $APPLE_PASSWORD ./build/dist/todoforge_0.1.38_darwin_all_desktop.dmg
 fi
