4eab9c79191b051248f1468749c5329895c9fc68
Kyle Unverferth <kyle@kyleu.com>
2025-03-29 09:43:37 -0600

    Minor cleanups and consolidation

--- app/state.go
+++ app/state.go
@@ -9,6 +9,7 @@ import (
 	"github.com/kyleu/todoforge/app/lib/database"
 	"github.com/kyleu/todoforge/app/lib/filesystem"
 	"github.com/kyleu/todoforge/app/lib/graphql"
+	"github.com/kyleu/todoforge/app/lib/log"
 	"github.com/kyleu/todoforge/app/lib/telemetry"
 	"github.com/kyleu/todoforge/app/lib/theme"
 	"github.com/kyleu/todoforge/app/util"
@@ -107,4 +108,24 @@ func Bootstrap(bi *BuildInfo, cfgDir string, port uint16, debug bool, logger uti
 	st.Services = svcs
 
 	return st, nil
+}
+
+func BootstrapRunDefault[T any](bi *BuildInfo, fn func(as *State, logger util.Logger) (T, error)) (T, error) {
+	logger, _ := log.InitLogging(false)
+	as, err := Bootstrap(bi, util.ConfigDir, 0, false, logger)
+	if err != nil {
+		var dflt T
+		return dflt, err
+	}
+	ret, err := fn(as, logger)
+	if err != nil {
+		var dflt T
+		return dflt, err
+	}
+	err = as.Close(context.Background(), logger)
+	if err != nil {
+		var dflt T
+		return dflt, err
+	}
+	return ret, nil
 }
