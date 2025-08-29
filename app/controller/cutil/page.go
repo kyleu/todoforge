8d16ece87b85336b10deb119048bf5bfc4c8e31b
Kyle Unverferth <kyle@kyleu.com>
2025-08-29 12:43:58 -0600

    UI improvements, cleaner rendered source

--- app/controller/cutil/page.go
+++ app/controller/cutil/page.go
@@ -18,6 +18,7 @@ import (
 	"github.com/kyleu/todoforge/app/lib/theme"
 	"github.com/kyleu/todoforge/app/lib/user"
 	"github.com/kyleu/todoforge/app/util"
+	"github.com/kyleu/todoforge/assets"
 )
 
 const (
@@ -184,4 +185,12 @@ func (p *PageState) MainClasses() string {
 		ret = append(ret, "nomenu")
 	}
 	return util.StringJoin(ret, " ")
+}
+
+func (p *PageState) AddHeaderScript(path string, deferFlag bool) {
+	p.HeaderContent += "\n  " + assets.ScriptElement(path, deferFlag)
+}
+
+func (p *PageState) AddHeaderStylesheet(path string) {
+	p.HeaderContent += "\n  " + assets.StylesheetElement(path)
 }
